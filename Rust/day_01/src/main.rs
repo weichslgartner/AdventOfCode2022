e3763d50ed14342a55027b10aea350acad4fe419
weichslgartner <weichslgartner@gmail.com>
2022-12-01 09:02:42 +0100

    day 01 rust clean up 1

--- Rust/day_01/src/main.rs
+++ Rust/day_01/src/main.rs
@@ -11,21 +11,19 @@ fn parse(input: &str) -> Vec<i32> {
         .collect::<Vec<i32>>()
 }
 
-fn part1(input: &str) -> i32 {
-    let parsed = input.split("\n\n").collect::<Vec<&str>>();
-    let res: i32 = parsed.into_iter().map(|y| parse_element(y)).max().unwrap();
-    return res;
+fn part1(input: &Vec<i32>) -> i32 {
+    *input.iter().max().unwrap()
 }
 
-fn part2(input: &str) -> i32 {
-    let mut res: Vec<i32> = parse(input);
-    res.sort();
-    res.reverse();
-    res[0..3].iter().sum()
+fn part2(input: &mut Vec<i32>) -> i32 {
+    input.sort();
+    input.reverse();
+    input[0..3].iter().sum()
 }
 
 fn main() {
     let input = include_str!("../../../inputs/input_01.txt").trim_end();
-    println!("Part 1: {}", part1(&input));
-    println!("Part 2: {}", part2(&input));
+    let mut parsed_input = parse(input);
+    println!("Part 1: {}", part1(&parsed_input));
+    println!("Part 2: {}", part2(&mut parsed_input));
 }
