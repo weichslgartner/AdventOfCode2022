88da9f8b92309aeb4d372c92e6270df2c1d00298
weichslgartner <weichslgartner@gmail.com>
2022-12-01 09:41:43 +0100

    day 01 rust refactor

--- Rust/day_01/src/main.rs
+++ Rust/day_01/src/main.rs
@@ -1,5 +1,6 @@
 fn parse(input: &str) -> Vec<i32> {
     input
+        .trim()
         .split("\n\n")
         .map(|y| y.split('\n').map(|e| e.parse::<i32>().unwrap()).sum())
         .collect::<Vec<i32>>()
@@ -10,13 +11,13 @@ fn part1(input: &Vec<i32>) -> i32 {
 }
 
 fn part2(input: &mut Vec<i32>) -> i32 {
-    input.sort();
-    input.reverse();
+    // if partial sort existed, would be better here
+    input.sort_unstable_by(|a, b| b.cmp(a));
     input[0..3].iter().sum()
 }
 
 fn main() {
-    let input = include_str!("../../../inputs/input_01.txt").trim_end();
+    let input = include_str!("../../../inputs/input_01.txt");
     let mut parsed_input = parse(input);
     println!("Part 1: {}", part1(&parsed_input));
     println!("Part 2: {}", part2(&mut parsed_input));
