3349c10319deb14c3714ef0aefb1d3d9c301c4df
weichslgartner <weichslgartner@gmail.com>
2022-12-02 15:32:16 +0100

    day 02 refactor python

--- Python/day_02.py
+++ Python/day_02.py
@@ -1,27 +1,34 @@
+from enum import Enum
 from functools import reduce
 from typing import List
 
 from aoc import get_lines
 
+
+class Result(Enum):
+    LOSE = 0
+    DRAW = 3
+    WIN = 6
+
+
 points = {'X': 1, 'Y': 2, 'Z': 3}
 
-second = {'X': 0, 'Y': 3, 'Z': 6}
+second = {'X': Result.LOSE.value, 'Y': Result.DRAW.value, 'Z': Result.WIN.value}
 
 win = ["A Y", "B Z", "C X"]
 draw = ["A X", "B Y", "C Z"]
 lose = ["A Z", "B X", "C Y"]
 
+pt2res = {Result.LOSE.value: lose, Result.DRAW.value: draw, Result.WIN.value: win}
+
 
 def convert(line: str) -> str:
-    if second[line[-1]] == 0:
-        return next(filter(lambda x: x[0] == line[0], lose))
-    if second[line[-1]] == 3:
-        return next(filter(lambda x: x[0] == line[0], draw))
-    return next(filter(lambda x: x[0] == line[0], win))
+    return next(x for x in pt2res[second[line[-1]]] if x[0] == line[0])
 
 
 def part_1(lines: List[str]) -> int:
-    return reduce(lambda accu, line: accu + points[line[-1]] + (line in win) * 6 + (line in draw) * 3, lines, 0)
+    return reduce(lambda accu, line: accu + points[line[-1]] + (line in win) * Result.WIN.value + (
+                line in draw) * Result.DRAW.value, lines, 0)
 
 
 def part_2(lines: List[str]) -> int:
