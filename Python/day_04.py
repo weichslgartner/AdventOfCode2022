74640afb7a21e9b7410e50348b61dc74452aab1f
weichslgartner <weichslgartner@gmail.com>
2022-12-04 10:35:51 +0100

    day04 python

---
+++ Python/day_04.py
@@ -0,0 +1,63 @@
+from aoc import get_lines, line_to_int
+
+test = """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+3-4,2-4
+4-4,4-88"""
+
+
+def parse_input(lines):
+    pairs = []
+    for line in lines:
+        tokenz = line.split(',')
+        pairs.append([line_to_int(p, '-') for p in tokenz])
+    return pairs
+
+
+def part_1(pairs):
+    sum = 0
+    for pair in pairs:
+        p1, p2 = pair
+        if complete_overlap(p1, p2):
+            sum += 1
+    return sum
+
+
+def complete_overlap(p1, p2):
+    return (p2[1] <= p1[1] and p2[0] >= p1[0]) or (p1[1] <= p2[1] and p1[0] >= p2[0])
+
+
+def partial_overlap(p1, p2):
+    if p2[0] < p1[0]:
+        p1, p2 = p2, p1
+    if p2[0] <= p1[1]:
+        return True
+    return False
+
+
+def part_2(pairs):
+    sum = 0
+    for pair in pairs:
+        p1, p2 = pair
+        if partial_overlap(p1, p2):
+            sum += 1
+
+            # print(pair)
+    return sum
+
+
+def main():
+    lines = get_lines("input_04.txt")
+    # lines = test.splitlines()
+    # print(lines)
+    pairs = parse_input(lines)
+    print("Part 1:", part_1(pairs))  # 508 too low
+    print("Part 2:", part_2(pairs))
+
+
+if __name__ == '__main__':
+    main()
