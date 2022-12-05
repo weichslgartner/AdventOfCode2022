71b9380cb9e29da03205d2bd634c875c51a44a4f
weichslgartner <weichslgartner@gmail.com>
2022-12-05 12:34:47 +0100

    day05  rust refactor 1

--- Rust/day_05/src/main.rs
+++ Rust/day_05/src/main.rs
@@ -1,4 +1,8 @@
 fn parse(input: &str) -> (Vec<Vec<char>>, Vec<Vec<usize>>) {
+    (parse_stacks(input), parse_moves(input))
+}
+
+fn parse_stacks(input: &str) -> Vec<Vec<char>> {
     let mut stacks: Vec<Vec<char>> = Vec::with_capacity(1);
     for line in input.lines().filter(|x| x.contains('[')) {
         for (idx, c) in line.chars().enumerate() {
@@ -10,59 +14,51 @@ fn parse(input: &str) -> (Vec<Vec<char>>, Vec<Vec<usize>>) {
             }
         }
     }
-
-    (
-        stacks.into_iter().filter(|b| !b.is_empty()).collect(),
-        input
-            .lines()
-            .filter(|x| x.contains("move"))
-            .map(|x| x.split(' '))
-            .map(|x| {
-                x.into_iter()
-                    .filter(|x| x.chars().next().unwrap().is_ascii_digit())
-                    .map(|x| x.parse().unwrap())
-                    .collect()
-            })
-            .collect(),
-    )
+    stacks.into_iter().filter(|b| !b.is_empty()).collect()
 }
 
-fn extract_top_name(stacks: &mut Vec<Vec<char>>) -> String {
-    let mut result = String::new();
-    for s in stacks {
-        result.push(s[s.len() - 1]);
-    }
-    result
+fn parse_moves(input: &str) -> Vec<Vec<usize>> {
+    input
+        .lines()
+        .filter(|x| x.contains("move"))
+        .map(|x| x.split(' '))
+        .map(|x| {
+            x.into_iter()
+                .filter(|x| x.chars().next().unwrap().is_ascii_digit())
+                .map(|x| x.parse().unwrap())
+                .collect()
+        })
+        .collect()
 }
 
-fn part1(stacks: &mut Vec<Vec<char>>, moves: &[Vec<usize>]) -> String {
+fn extract_top_name(stacks: &[Vec<char>]) -> String {
+    stacks.iter().map(|s| s[s.len() - 1]).collect()
+}
+
+fn part1(stacks: &mut [Vec<char>], moves: &[Vec<usize>]) -> String {
     moves.iter().for_each(|m| {
-        let times = m[0];
-        let src = m[1];
-        let dst = m[2];
-        for _ in 0..times {
-            let el = stacks[src - 1].pop().unwrap();
-            stacks[dst - 1].push(el);
+        if let [times, src, dst] = &m[..] {
+            for _ in 0..*times {
+                let el = stacks[src - 1].pop().unwrap();
+                stacks[dst - 1].push(el);
+            }
         }
     });
     extract_top_name(stacks)
 }
 
-
-
-fn part2(stacks: &mut Vec<Vec<char>>, moves: &[Vec<usize>]) -> String {
+fn part2(stacks: &mut [Vec<char>], moves: &[Vec<usize>]) -> String {
     moves.iter().for_each(|m| {
-        let times = m[0];
-        let src = m[1];
-        let dst = m[2];
-        let mut elements: Vec<char> = vec![];
-        for _ in 0..times {
-            elements.push(stacks[src - 1].pop().unwrap());
-        }
+        if let [times, src, dst] = &m[..] {
+            let mut elements: Vec<char> = vec![];
+            for _ in 0..*times {
+                elements.push(stacks[src - 1].pop().unwrap());
+            }
 
-        elements.reverse();
-        for e in elements {
-            stacks[dst - 1].push(e);
+            elements.reverse();
+            for e in elements {
+                stacks[dst - 1].push(e);
+            }
         }
     });
     extract_top_name(stacks)
