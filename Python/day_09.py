ff861c733a799882254142a03b4af9439291c9fe
weichslgartner <weichslgartner@gmail.com>
2022-12-09 11:40:11 +0100

    day09 python refactor

--- Python/day_09.py
+++ Python/day_09.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 from aoc import get_lines, Point
 
@@ -34,15 +34,23 @@ def solve(commands: List[Tuple[str, int]], length: int = 10) -> int:
     positions = {i: Point(0, 0) for i in range(length)}
     tail_set = set()
     for direct, steps in commands:
-        p = dir_to_point(direct)
+        direct_point = dir_to_point(direct)
         for _ in range(steps):
-            positions[0] = Point(positions[0].x + p.x, positions[0].y + p.y)
-            for i in range(1, length):
-                if positions[i - 1] not in get_neighbours_8(positions[i]):
-                    positions[i] = Point(positions[i].x + sign(positions[i - 1].x - positions[i].x),
-                                         positions[i].y + sign(positions[i - 1].y - positions[i].y))
+            positions[0] = move_head(positions[0], direct_point)
+            move_body_and_tail(positions)
             tail_set.add(positions[length - 1])
     return len(tail_set)
+
+
+def move_body_and_tail(positions: Dict[int, Point]):
+    for i in range(1, len(positions)):
+        if positions[i - 1] not in get_neighbours_8(positions[i]):
+            positions[i] = Point(positions[i].x + sign(positions[i - 1].x - positions[i].x),
+                                 positions[i].y + sign(positions[i - 1].y - positions[i].y))
+
+
+def move_head(head: Point, direct_point: Point) -> Point:
+    return Point(head.x + direct_point.x, head.y + direct_point.y)
 
 
 def part_1(commands: List[Tuple[str, int]]) -> int:
@@ -56,8 +64,8 @@ def part_2(commands: List[Tuple[str, int]]) -> int:
 def main():
     lines = get_lines("input_09.txt")
     commands = parse_input(lines)
-    print("Part 1:", solve(commands, 2))    # 5874
-    print("Part 2:", solve(commands, 10))   # 2467
+    print("Part 1:", solve(commands, 2))  # 5874
+    print("Part 2:", solve(commands, 10))  # 2467
 
 
 if __name__ == '__main__':
