8dbfc5183cc0087854f27fae6ae6d8cc3c30c9c2
weichslgartner <weichslgartner@gmail.com>
2022-12-12 11:47:10 +0100

    day_12 Python type hints

--- Python/day_12.py
+++ Python/day_12.py
@@ -1,13 +1,14 @@
 import heapq
 import sys
-from collections import deque, defaultdict
+from collections import defaultdict
+from typing import List, Tuple, Dict, Set
 
 from aoc import get_lines, Point, get_neighbours_4, manhattan_distance
-from pathlib import Path
 
 
-def parse_input(lines):
-    grid = [[1 for c in line] for line in lines]
+def parse_input(lines: List[str]) -> Tuple[List[List[int]], Point, Point, Point]:
+    grid = [[1 for _ in line] for line in lines]
+    start, goal = None, None
     for y, line in enumerate(lines):
         for x, c in enumerate(line):
             grid[y][x] = ord(c)
@@ -17,49 +18,15 @@ def parse_input(lines):
             if c == 'E':
                 goal = Point(x, y)
                 grid[y][x] = ord('z')
-
     return grid, start, goal, Point(len(lines[0]), len(lines))
 
 
-def part_1(grid, start, goal, maxp):
-    assert (grid[goal.y][goal.x] == ord('z'))
-    assert (grid[start.y][start.x] == ord('a'))
-
-    print(start, goal)
-    deq = []
-    deq.append((manhattan_distance(start, goal), 0, start))
-    best = sys.maxsize
-    visited = set()
-    while len(deq) > 0:
-        # print(len(visited), len(deq))
-        _, dist, cur = heapq.heappop(deq)
-        if dist > best:
-            continue
-        # print(_,dist,cur)
-        if cur == goal:
-            # return dist
-            best = min(best, dist)
-        visited.add(cur)
-        neighbors = get_neighbours_4(cur, maxp)
-        for n in neighbors:
-            if grid[n.y][n.x] <= grid[cur.y][cur.x] + 1 and n not in visited:
-                heapq.heappush(deq, (manhattan_distance(n, goal), dist + 1, n))
-    return best
-
-
-def part_1(grid, start, goal, maxp) -> int:
-    return a_star(grid, start, goal, maxp, True)
-
-def part_2(grid, start, p_target, maxp) -> int:
-    return a_star(grid, start, p_target, maxp,False)
-
-def a_star(grid, start, p_target, maxp, is_part1) -> int:
-    costs, f_costs, in_queue, queue = init_a_start(grid, p_target, start,is_part1)
+def a_star(grid: List[List[int]], start: Point, goal: Point, maxp: Point, is_part1: bool) -> int:
+    costs, f_costs, in_queue, queue = init_a_start(grid, goal, start, is_part1)
     while len(queue) > 0:
-        assert (len(queue) == len(in_queue))
         _, cur = heapq.heappop(queue)
         in_queue.remove(cur)
-        if cur == p_target:
+        if cur == goal:
             return costs[cur]
         for n in get_neighbours_4(cur, maxp):
             if grid[n.y][n.x] > grid[cur.y][cur.x] + 1:
@@ -67,13 +34,15 @@ def a_star(grid, start, p_target, maxp, is_part1) -> int:
             t_costs = costs[cur] + 1
             if t_costs < costs[n]:
                 costs[n] = t_costs
-                f_costs[n] = t_costs + manhattan_distance(n, p_target)
+                f_costs[n] = t_costs + manhattan_distance(n, goal)
                 if n not in in_queue:
                     heapq.heappush(queue, (f_costs[n], n))
                     in_queue.add(n)
+    return sys.maxsize
 
 
-def init_a_start(grid, p_target, start, is_part_1: bool):
+def init_a_start(grid: List[List[int]], p_target: Point, start: Point, is_part_1: bool) -> \
+        Tuple[Dict[Point, int], Dict[Point, int], Set[Point], List[Tuple[int, Point]]]:
     costs = defaultdict(lambda: sys.maxsize)
     f_costs = defaultdict(lambda: sys.maxsize)
     in_queue = {start}
@@ -81,7 +50,7 @@ def init_a_start(grid, p_target, start, is_part_1: bool):
     costs[start] = 0
     f_costs[start] = manhattan_distance(start, p_target)
     if is_part_1:
-        queue.append((manhattan_distance(start, p_target),start))
+        queue.append((manhattan_distance(start, p_target), start))
         return costs, f_costs, in_queue, queue
     for y, line in enumerate(grid):
         for x, c in enumerate(line):
@@ -92,6 +61,14 @@ def init_a_start(grid, p_target, start, is_part_1: bool):
                 f_costs[p] = manhattan_distance(p, p_target)
                 costs[p] = 0
     return costs, f_costs, in_queue, queue
+
+
+def part_1(grid: List[List[int]], start: Point, goal: Point, maxp: Point) -> int:
+    return a_star(grid, start, goal, maxp, True)
+
+
+def part_2(grid: List[List[int]], start: Point, goal: Point, maxp: Point) -> int:
+    return a_star(grid, start, goal, maxp, False)
 
 
 def main():
