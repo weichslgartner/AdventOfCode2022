0e993b4424452950738874592b55580f2cb917c8
weichslgartner <weichslgartner@gmail.com>
2022-12-15 22:21:50 +0100

    day 15 rust

--- Python/day_15.py
+++ Python/day_15.py
@@ -1,11 +1,15 @@
 from aoc import *
+import multiprocess
+
 
 
 def parse_input(lines):
-    return [[Point(ints[0], ints[1]), Point(ints[2], ints[3]), manhattan_distance(Point(ints[0], ints[1]), Point(ints[2], ints[3]))] for ints in map(extract_all_ints, lines)]
+    return list(map(lambda p: [p[0], p[1], manhattan_distance(*p)],
+                         map(lambda ints: [Point(ints[0], ints[1]), Point(ints[2], ints[3])],
+                                  map(extract_all_ints, lines))))
 
 
-def part_1(pb, target=2000000):
+def part_1(pb, target=2_000_000):
     free_set = set()
     for sensor, beacon, mh in pb:
         dist = mh - manhattan_distance(sensor, Point(sensor.x, target))
@@ -31,15 +35,13 @@ def is_in_area(sensor, upper, target, mh):
     return True, max(sensor.x - dist, 0), min(sensor.x + dist, upper)
 
 
-def part_2(pb, upper=4000000):
-    for y in range(0, upper + 1):
-        x = 0
-        while x < upper:
-            ranges = []
-            for sensor, beacon, mh in pb:
-                isin, x_min, x_max = is_in_area(sensor, upper, y, mh)
-                if isin:
-                    ranges.append([x_min, x_max])
+def part_2(pb, upper=20):
+        for y in range(0, upper + 1):
+            x = 0
+            ranges = list(
+                map(lambda x: (x[1], x[2]),
+                         filter(lambda x: x[0], map(lambda x: is_in_area(x[0], upper, y, x[2]), pb))))
+
             for x_min, x_max in sorted(ranges):
                 if x_min <= x <= x_max:
                     x = x_max
@@ -48,11 +50,11 @@ def part_2(pb, upper=4000000):
             if x != upper:
                 return (x + 1) * 4000000 + y
 
-    return None
+        return None
 
 
 def main():
-    lines = get_lines("input_15.txt")
+    lines = get_lines("input_15_test.txt")
     pb = parse_input(lines)
     print("Part 1:", part_1(pb))  # 4748135
     print("Part 2:", part_2(pb))  # 13743542639657
---
+++ Rust/day_15/src/main.rs
@@ -0,0 +1,113 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+
+#[derive(Eq, Hash, PartialEq, Debug, Clone, Copy)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+fn manhattan_distance(a: Point, b: Point) -> i32 {
+    ((a.x).abs_diff(b.x) + (a.y).abs_diff(b.y)) as i32
+}
+fn parse(input: &str) -> Vec<(Point, i32)> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"-?\d+").unwrap();
+    }
+    input
+        .lines()
+        .map(|line| {
+            RE.find_iter(line)
+                .filter_map(|digits| digits.as_str().parse::<i32>().ok())
+                .collect::<Vec<_>>()
+        })
+        .map(|v| (Point { x: v[0], y: v[1] }, Point { x: v[2], y: v[3] }))
+        .map(|(p1, p2)| (p1, manhattan_distance(p1, p2)))
+        .collect::<Vec<(Point, i32)>>()
+}
+
+fn is_in_area(sensor: Point, upper: Option<i32>, target: i32, mh: i32) -> (bool, i32, i32) {
+    let dist = mh
+        - manhattan_distance(
+            sensor,
+            Point {
+                x: sensor.x,
+                y: target,
+            },
+        );
+    if dist < 0 {
+        return (false, 0, 0);
+    }
+    if let Some(upper) = upper {
+        let x_min = 0.max(sensor.x - mh);
+        let x_max = upper.min(sensor.x + mh);
+        if x_min > upper || x_max < 0 {
+            return (false, 0, 0);
+        }
+        return (true, 0.max(sensor.x - dist), upper.min(sensor.x + dist));
+    }
+    (true, sensor.x - dist, sensor.x + dist)
+}
+
+fn get_sorted_ranges(pb: &[(Point, i32)], upper: Option<i32>, y: i32) -> Vec<(i32, i32)> {
+    let mut ranges: Vec<_> = pb
+        .iter()
+        .map(|(sensor, mh)| is_in_area(*sensor, upper, y, *mh))
+        .filter(|(valid, _, _)| *valid)
+        .map(|(_, x_in, x_max)| (x_in, x_max))
+        .collect();
+    ranges.sort();
+    ranges
+}
+
+fn part1(pb: &Vec<(Point, i32)>, target: i32) -> Option<u64> {
+    let ranges = get_sorted_ranges(&pb, None, target);
+    let mut free_spaces: u64 = 0;
+    let mut x = None;
+    for (x_min, x_max) in ranges {
+        match x {
+            Some(old_x) => {
+                if old_x > x_max {
+                    //do nothing
+                } else if old_x >= x_min && old_x <= x_max {
+                    free_spaces += (x_max - old_x) as u64;
+                }
+                if old_x < x_max {
+                    x = Some(x_max);
+                }
+            }
+            _ => {
+                free_spaces += (x_max - x_min) as u64;
+                x = Some(x_max)
+            }
+        }
+    }
+    Some(free_spaces)
+}
+
+fn part2(pb: &Vec<(Point, i32)>, upper: i32) -> Option<u64> {
+    for y in 0..=upper {
+        let mut x = 0;
+
+        let ranges = get_sorted_ranges(pb, Some(upper), y);
+        for (x_min, x_max) in ranges {
+            if x >= x_min && x <= x_max {
+                x = x_max;
+            }
+            if x >= upper {
+                break;
+            }
+        }
+        if x != upper {
+            return Some((x + 1) as u64 * 4000000 + y as u64);
+        }
+    }
+    None
+}
+
+fn main() {
+    let input = include_str!("../../../inputs/input_15.txt");
+    let pb = parse(input);
+    println!("Part 1: {}", part1(&pb, 2_000_000).unwrap());
+    println!("Part 2: {}", part2(&pb, 4_000_000).unwrap());
+}
