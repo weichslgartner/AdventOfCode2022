02a1c46ad3e70ab26bff2d388dbe555af01ae4df
weichslgartner <weichslgartner@gmail.com>
2022-12-20 11:58:03 +0100

    paython day 20 refactor

--- Python/day_20.py
+++ Python/day_20.py
@@ -1,35 +1,33 @@
+from typing import List, Tuple
+
 from aoc import get_lines
 
 decryption_key = 811589153
 
 
-def parse_input(lines):
+def parse_input(lines: List[str]) -> List:
     return list(enumerate(map(int, lines)))
 
 
-def solve(lines, rounds):
+def solve(lines: List[Tuple[int, int]], rounds: int) -> int:
     length = len(lines)
     elements = lines.copy()
     for _ in range(rounds):
         for n in elements:
             old_index = lines.index(n)
-            i = n[1]
-            if i == 0:
+            if n[1] == 0:
                 continue
-            elif old_index + i < 0 or old_index + i >= length:
-                new_index = (old_index + i) % (length - 1)
-            else:
-                new_index = (old_index + i)
-            lines.insert(new_index if new_index else length - 1, lines.pop(old_index))
+            new_index = (old_index + n[1]) % (length - 1)
+            lines.insert(new_index, lines.pop(old_index))
     idx = lines.index(list(filter(lambda x: x[1] == 0, lines)).pop())
     return sum(map(lambda x: x[1], (lines[(idx + grove) % length] for grove in [1000, 2000, 3000])))
 
 
-def part_1(lines):
+def part_1(lines: List[Tuple[int, int]]) -> int:
     return solve(lines, 1)
 
 
-def part_2(lines):
+def part_2(lines: List[Tuple[int, int]]) -> int:
     lines = list(map(lambda x: (x[0], x[1] * decryption_key), lines))
     return solve(lines, 10)
 
