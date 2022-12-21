48d88e938017475f6a7b3afe928ef7bed13ddc14
weichslgartner <weichslgartner@gmail.com>
2022-12-21 19:46:46 +0100

    day 16 python bugfix

--- Python/day_16.py
+++ Python/day_16.py
@@ -7,7 +7,7 @@ from typing import List, Set
 
 from aoc import *
 
-
+max_pressure = 0
 @dataclass
 class Valve:
     name: str = 0
@@ -50,12 +50,7 @@ def part_1(valves, time=29):
     visited = {}
     while len(queue) > 0:
         cur = queue.popleft()
-        if cur.pos not in cur.visited:
-            if valve_dict[cur.pos].pressure > 0:
-                # cur.time += 1
-                cur.pressure_sum += cur.pressure
-            cur.visited.add(cur.pos)
-            cur.pressure += valve_dict[cur.pos].pressure
+        open_valve(cur_state=cur, valve_dict=valve_dict)
         costs = [((time - cur.time - abs(dists[cur.pos][val.name]) - 1) * val.pressure, val)
                  for val in filter(lambda v: v.name not in cur.visited and v.pressure > 0, valves)]
         # print(sorted(costs, key=lambda x: x[0], reverse=True))
@@ -63,12 +58,9 @@ def part_1(valves, time=29):
             travel_time = dists[cur.pos][cost[1].name]
             if cur.time + travel_time > time:
                 continue
-            next = State(pos=cost[1].name, time=cur.time + travel_time + 1, pressure=cur.pressure,
-                         pressure_sum=cur.pressure_sum + travel_time * cur.pressure, visited=copy(cur.visited),
-                         # history=copy(cur.history)
-                         )
-            # next.history.append((cur.time + travel_time, cost[1].name, cur.pressure, cur.pressure_sum))
-            next.visited
+
+            next = get_next_state(cur, cost[1].name,travel_time)
+            next.pressure_sum += cur.pressure * travel_time
             key = ''.join(sorted(next.visited))
             # print(key,next.time )
             if key in visited:
@@ -111,126 +103,136 @@ def find_shortest_paths(valve_dict, start, valves):
     return dist
 
 
-def part_2(valves, time=25):
+def part_2(valves, time=26):
+    global max_pressure
     valve_dict = {v.name: v for v in valves}
+    max_pressure = sum( v.pressure for v in valves)
+    max_pressure = time*max_pressure
+    print(max_pressure,max_pressure/time)
+    print([v.pressure for v in valves if v.pressure > 0])
     dists = {valve.name: find_shortest_paths(valve_dict, valve, valves) for valve in valves}
-    # print(dists)
     queue = deque()
     best = 0
     queue.append(
-        (State(pos="AA", time=0, pressure=0, pressure_sum=0), State(pos="AA", time=0, pressure=0, pressure_sum=0)))
+        (State(pos="AA", time=1, pressure=0, pressure_sum=0), State(pos="AA", time=1, pressure=0, pressure_sum=0)))
     visited = {}
     while len(queue) > 0:
         me, elephant = queue.popleft()
+        assert (elephant.pressure_sum < max_pressure)
+        elephant_old = copy(elephant)
         new_elephant = False
         new_me = False
-        cur_time = None
+        # cur_time = None
         if me.time < elephant.time:
-            open_valve(me, valve_dict)
             cur_time = me.time
-            # elephant.time = me.time
-            elephant.visited |= me.visited
-            elephant.pressure = me.pressure
-            me.pressure = open_to_pressure(me.visited, valve_dict)
-            elephant.pressure = open_to_pressure(elephant.visited, valve_dict)
-            elephant.pressure_sum = me.pressure_sum
+            open_valve(me, valve_dict)
+            assert(elephant.pressure_sum == elephant_old.pressure_sum)
             new_me = True
         elif me.time > elephant.time:
             cur_time = elephant.time
             open_valve(elephant, valve_dict)
-            me.pressure = elephant.pressure
-            me.visited |= elephant.visited
-            me.pressure_sum = elephant.pressure_sum
-            me.pressure = open_to_pressure(me.visited, valve_dict)
-            elephant.pressure = open_to_pressure(elephant.visited, valve_dict)
-
             new_elephant = True
         else:
-            inc_el = open_valve(elephant, valve_dict)
-            inc_me = open_valve(me, valve_dict)
-            # if inc_me == 22 and inc_el==3:
-            #     print("alarm")
+            open_valve(elephant, valve_dict)
+            open_valve(me, valve_dict)
+            assert (elephant.time == me.time)
             cur_time = elephant.time
-            me.pressure += inc_el
-            me.visited |= elephant.visited
-            elephant.visited |= me.visited
-            elephant.pressure = open_to_pressure(elephant.visited, valve_dict)
-            me.pressure = open_to_pressure(me.visited, valve_dict)
-
-
             new_elephant = True
             new_me = True
-        if me.pressure > 81:
-            print("debug")
-        # if me.pressure != elephant.pressure:
-        #     print("alarm")
-        # print(me,elephant)
-        # print(sorted(costs, key=lambda x: x[0], reverse=True))
-        elephant_coice = list(filter(lambda v: v.name not in me.visited and v.pressure > 0,
-                                     valves)) if new_elephant else [elephant]
-        me_choice = list(filter(lambda v: v.name not in me.visited and v.pressure > 0, valves)) if new_me else [me]
+        elephant_coice = list(
+            filter(lambda v: v.name not in me.visited and v.pressure > 0 and v.name not in elephant.visited,
+                   valves)) if new_elephant else [elephant]
+        me_choice = list(
+            filter(lambda v: v.name not in me.visited and v.pressure > 0 and v.name not in elephant.visited,
+                   valves)) if new_me else [me]
         for next_elephant in elephant_coice:
             for next_me in me_choice:
-                #        if me.pressure != elephant.pressure:
-                #           print("alarm")
-
                 if new_me:
-                    travel_time = dists[me.pos][next_me.name] + 1
-                    if me.time + travel_time > time:
+                    travel_time = dists[me.pos][next_me.name]
+                    if cur_time + travel_time > time:
                         continue
-                    next_me_state = get_next_state(me, next_me, travel_time)
-                    # if next_me_state.pos == "HH":
-                    #    print("debug")
+                    next_me_state = get_next_state(me, next_me.name, travel_time)
                 else:
-                    next_me_state = me
+                    next_me_state = copy(me)
                 if new_elephant:
-                    travel_time = dists[elephant.pos][next_elephant.name] + 1
-                    if elephant.time + travel_time > time:
+                    travel_time = dists[elephant.pos][next_elephant.name]
+                    if cur_time + travel_time > time:
                         continue
-                    next_elephant_state = get_next_state(elephant, next_elephant, travel_time)
+                    next_elephant_state = get_next_state(elephant, next_elephant.name, travel_time)
+                    assert (next_elephant_state.pressure_sum < max_pressure)
+
                 else:
-                    next_elephant_state = elephant
+                    next_elephant_state = copy(elephant)
+                    if next_elephant_state.pressure_sum > max_pressure:
+                        print("debug")
+                    assert (next_elephant_state.pressure_sum < max_pressure)
+
                 if next_elephant_state.pos == next_me_state.pos:
                     continue
                 time_until_next = min(next_me_state.time - cur_time, next_elephant_state.time - cur_time)
-                next_elephant_state.pressure_sum += next_elephant_state.pressure * time_until_next
-                next_me_state.pressure_sum += next_me_state.pressure * time_until_next
+                if time_until_next > 25:
+                    print("debug")
+                assert (time_until_next >= 0)
+                assert(cur_time + time_until_next <=time )
+                #            next.pressure_sum += cur.pressure * travel_time
+                before = next_elephant_state.pressure_sum
+                assert( next_elephant_state.pressure_sum < max_pressure)
 
+                next_elephant_state.pressure_sum += elephant.pressure * time_until_next
+                if( next_elephant_state.pressure_sum > max_pressure):
+                    print(elephant)
+                next_me_state.pressure_sum += me.pressure * time_until_next
+                if next_elephant_state.pressure_sum -before> 800:
+                    print("debug")
                 queue.append((next_me_state, next_elephant_state))
 
-        new = me.pressure_sum + me.pressure * (time + 1 - me.time)
+        new = me.pressure_sum + me.pressure * (time + 1 - me.time) + elephant.pressure_sum + elephant.pressure * (
+                    time + 1 - elephant.time)
         if new > best:
-            print(best, me, elephant)
             best = new
+            print(best, me.pressure, elephant.pressure, me, elephant)
+
     return best
 
 
-def get_next_state(me, next_me, travel_time):
-    next = State(pos=next_me.name, time=me.time + travel_time, pressure=me.pressure,
-                 pressure_sum=me.pressure_sum, visited=copy(me.visited),
-                 history=copy(me.history)
+def get_next_state(cur, next_me_name, travel_time):
+    next = State(pos=next_me_name, time=cur.time + travel_time + 1, pressure=cur.pressure,
+                 pressure_sum=copy(cur.pressure_sum), visited=copy(cur.visited),
+                 history=copy(cur.history)
                  )
-    next.history.append((me.time + travel_time, next_me.name, me.pressure, me.pressure_sum))
+    next.history.append((cur.time + travel_time, next_me_name, cur.pressure, cur.pressure_sum))
     return next
 
 
+# next = State(pos=cost[1].name, time=cur.time + travel_time + 1, pressure=cur.pressure,
+#              pressure_sum=cur.pressure_sum + travel_time * cur.pressure, visited=copy(cur.visited),
+#              # history=copy(cur.history)
+#              )
+# next.history.append((cur.time + travel_time, cost[1].name, cur.pressure, cur.pressure_sum))
+
 def open_valve(cur_state, valve_dict):
+    global max_pressure
     if cur_state.pos not in cur_state.visited:
         if valve_dict[cur_state.pos].pressure > 0:
             # if cur_state.pressure > 0:
+            assert (cur_state.pressure_sum < max_pressure)
+
             cur_state.pressure_sum += cur_state.pressure
+            assert (cur_state.pressure_sum < max_pressure)
         cur_state.visited.add(cur_state.pos)
         cur_state.pressure += valve_dict[cur_state.pos].pressure
     return valve_dict[cur_state.pos].pressure
 
+
 def open_to_pressure(visited, valve_dict):
     return sum(valve_dict[v].pressure for v in visited)
+
 
 def main():
     lines = get_lines("input_16_test.txt")
     valves = parse_input(lines)
-    print("Part 1:", part_1(valves))
-    print("Part 2:", part_2(valves))  # too hi 2712
+  #  print("Part 1:", part_1(valves))
+    print("Part 2:", part_2(valves))  # too hi 2712 too low 1965
 
 
 if __name__ == '__main__':
